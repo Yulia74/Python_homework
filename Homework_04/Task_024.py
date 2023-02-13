e63b3cc01e6e409797674742bab898a7995251f9
[Yulia Lebedeva] <yl.75@mail.ru>
2023-02-13 20:07:40 +0500

    optimization task 024

--- Homework_04/Task_024.py
+++ Homework_04/Task_024.py
@@ -17,7 +17,7 @@ Input:
 
 Output:
 9
-'''
+
 
 n = int(input('Введите количество кустов: '))
 
@@ -37,3 +37,19 @@ if bush[0] + bush[1] + bush[-1] > max:
 	max = bush[0] + bush[1] + bush[-1]
         
 print(f'Максимальный урожай: {max}')
+
+2 вариант решения (оптимальный)
+'''
+n = int(input())
+arr = list()
+for i in range(n):
+    x = int(input())
+    arr.append(x)
+
+arr_count = list()
+for i in range(len(arr) - 1):
+    arr_count.append(arr[i - 1] + arr[i]+arr[i + 1])
+arr_count.append(arr[-2]+arr[-1]+arr[0])
+print(max(arr_count))   
+
+
