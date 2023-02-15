5b88dfe4088a749203f181ff8164dc0a25c2e5de
[Yulia Lebedeva] <yl.75@mail.ru>
2023-02-15 20:00:52 +0500

    added a different solution of the task

--- Homework_05/Task_028.py
+++ Homework_05/Task_028.py
@@ -9,7 +9,9 @@
 def sum(a, b):
     if a == 0:
         return b
-    return sum(a-1, b+1)
+    return sum(a-1, b+1)    #  или  return sum(a, b-1)+1
 
 
 print(sum(int(input('Введите число (а): ')), int(input('Введите число (b): '))))
+
+
