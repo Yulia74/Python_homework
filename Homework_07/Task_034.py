530b6ab540a2ae2d2d657002bcd0816862e9a2cf
[Yulia Lebedeva] <yl.75@mail.ru>
2023-02-23 21:01:45 +0500

    add another solution of the task

--- Homework_07/Task_034.py
+++ Homework_07/Task_034.py
@@ -48,7 +48,7 @@ else:
 
 
 3 вариант решения (через lambda)
-'''
+
 
 text = input('Винни Пух напиши стих: ').lower().split()# ввожу текст, перевожу все в нижний регистр,
                                                        # разделяю на элементы списка пробелом <class 'list'>
@@ -60,3 +60,47 @@ if all([f(i) == t for i in text]):
     print('Парам пам-пам')
 else:
     print('Пам парам')
+
+
+4 вариант решения задачи универсальный (со всеми гласными)
+'''
+stroka = 'пара-ра-рам рам-пaм-папам па-ра-па-дам'
+vowels = ['а', 'е', 'ё', 'и', 'й', 'о', 'у', 'ы', 'э', 'ю', 'я']
+phrases = stroka.split()
+if len(phrases) < 2:                               # проверка на количество фраз, чтобы их во фразе было >1
+    print('Количество фраз должно быть больше одной!')
+else:
+    countVowels = []
+
+for i in phrases:                                                  # # list.comprehension
+    countVowels.append(len([x for x in i if x.lower() in vowels])) 
+# print(countVowels)                                               # [4, 3, 4]
+# print(countVowels[0])                                            # 4
+# print(len(countVowels))                                          # 3
+# print(countVowels.count(countVowels[0]))                         # 2
+if countVowels.count(countVowels[0]) == len(countVowels):
+    print('Парам пам-пам')
+else:
+    print('Пам парам')
+
+'''
+список.comprehension
+
+for i in phrases:
+    countVowels.append(len([x for x in i if x.lower() in vowels]))
+
+если разложить строки list.comprehension, то в обычном виде код будет выглядеть так
+
+for i in phrases:
+    list=[]
+    for x in i:
+        if x.lower() in vowels:
+        list.append(x)            # list = ['a','a','a']
+            
+    countVowels.append(len(list)
+
+    
+countVowels.append(len([x for x in i if x.lower() in vowels]))
+эта строка звучит: в список countVowels, добавляем длину списка.comprehension 
+в которые сохраняем те буквы, которые есть в списке vowels
+'''
